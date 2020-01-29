809b30c8f6fb2fa8f13e807041ec9d004ac0eeaf
strophy <leon.white@dash.org>
2020-01-28 17:26:02 -0800

    Make file executable and deploy

--- dash.sh
+++ dash.sh
@@ -5,3 +5,6 @@ ytt -f ./config.yml -f conf \
   --file-mark insight.json:type=text-template \
   --file-mark dash.conf:type=text-template \
   --output-directory deploy
+
+cd deploy
+docker-compose up -d
