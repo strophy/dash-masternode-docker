e6ce787042081edd7970c7d2cdd2ca28b686aee0
strophy <leon.white@dash.org>
2020-01-28 17:20:56 -0800

    Switch to ytt

---
+++ dash.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Dash masternode docker-compose templating script
+
+ytt -f ./config.yml -f conf \
+  --file-mark insight.json:type=text-template \
+  --file-mark dash.conf:type=text-template \
+  --output-directory deploy
