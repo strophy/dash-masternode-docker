153f24f20433e2cf58db3faf61d49e9517e64349
strophy <leon.white@dash.org>
2020-01-28 18:37:52 -0800

    Rename output directory

--- dash.sh
+++ dash.sh
@@ -4,7 +4,7 @@
 ytt -f ./config.yml -f conf \
   --file-mark insight.json:type=text-template \
   --file-mark dash.conf:type=text-template \
-  --output-directory deploy
+  --output-directory masternode
 
-cd deploy
+cd masternode
 docker-compose up -d
