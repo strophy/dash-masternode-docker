1d6aa930083a1ea47bf25540319a94da34e2be6d
strophy <leon.white@dash.org>
2020-01-29 16:31:04 -0800

    Generate random RPC password

--- dash.sh
+++ dash.sh
@@ -4,6 +4,7 @@
 ytt -f ./config.yml -f conf \
   --file-mark insight.json:type=text-template \
   --file-mark dash.conf:type=text-template \
+  --data-value rpc_password=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32}) \
   --output-directory masternode
 
 cd masternode
